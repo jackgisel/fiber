ad7e681988d05f9c1823258893b78f0a33438217
Jack G <jackrgisel@gmail.com>
2023-01-01 19:21:06 -0800

    added DB connection

--- go.mod
+++ go.mod
@@ -4,4 +4,8 @@ module fiber
 
 go 1.16
 
-require github.com/gofiber/fiber/v2 v2.33.0
+require (
+	github.com/gofiber/fiber/v2 v2.33.0
+	gorm.io/driver/postgres v1.4.6 // indirect
+	gorm.io/gorm v1.24.3 // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,35 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+var DB *gorm.DB
+
+func init() {
+	db_name := os.Getenv("PGDATABASE")
+	db_host := os.Getenv("PGHOST")
+	db_pass := os.Getenv("PGPASSWORD")
+	db_port := os.Getenv("PGPORT")
+	db_user := os.Getenv("PGUSER")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", db_host, db_user, db_pass, db_name, db_port)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if (err != nil) {
+		log.Fatalln("Failed to connect to DB", err)
+	} else {
+		log.Println("Connected to the DB")
+	}
+
+	DB = db
+}
 
 func getPort() string {
 	port := os.Getenv("PORT")
